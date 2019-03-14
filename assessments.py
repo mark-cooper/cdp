863b1fe4a10d1268901bcd43f92f6ac4d3f462d3
Mark Cooper <markchristophercooper@gmail.com>
2019-03-14 15:35:25 -0700

    Import assessments

--- assessments.py
+++ assessments.py
@@ -6,6 +6,9 @@
 import csv
 import json
 import os
+import re
+from cdp import ArchivesSpace
+from time import sleep
 
 data = []
 target_resources = {}
@@ -13,6 +16,9 @@ target_resources = {}
 DATA_DIRECTORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'data', 'assessments')  # noqa
 base_path = os.path.dirname(os.path.realpath(__file__))
 resources = os.path.join(base_path, 'resources.csv')
+
+service = ArchivesSpace(ArchivesSpace.DEFAULT_CONFIG)
+service.ping()
 
 FIELDS_TO_DELETE = [
     'collections',
@@ -37,6 +43,20 @@ for assessment in os.listdir(DATA_DIRECTORY):
             if identifier in target_resources:
                 data = json.load(json_file)
                 rid = target_resources[identifier]
-                # TODO: delete fields from json
-                # TODO: replace old resource id with rid ['records']['ref]
-                print(rid)
+                repo_uri = data['repository']['ref']
+                path = f'{repo_uri}/assessments'
+
+                # delete fields not used for import
+                for field in FIELDS_TO_DELETE:
+                    del data[field]
+
+                # replace old resource id with rid ['records'][0]['ref]
+                for record in data['records']:
+                    if not 'resources' in record['ref']:
+                        continue
+                    updated_record_id = re.sub(r'(\d+)$', rid, record['ref'])
+                    record['ref'] = updated_record_id
+
+                print(f'Creating assessment for: {rid}')
+                service.post(path, data)
+                sleep(0.1)
