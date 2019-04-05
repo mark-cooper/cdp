2e908932c8e1b4464d9f6583b37d453e939ae4a8
Mark Cooper <markchristophercooper@gmail.com>
2019-04-05 09:23:48 -0700

    Date adjust for overnight process

--- sql/publish.sql
+++ sql/publish.sql
@@ -1,4 +1,4 @@
-UPDATE agent_corporate_entity SET publish = true, system_mtime = NOW() WHERE create_time >= CURDATE();
-UPDATE agent_family SET publish = true, system_mtime = NOW() WHERE create_time >= CURDATE();
-UPDATE agent_person SET publish = true, system_mtime = NOW() WHERE create_time >= CURDATE();
-UPDATE subject SET publish = true, system_mtime = NOW() WHERE create_time >= CURDATE();
+UPDATE agent_corporate_entity SET publish = true, system_mtime = NOW() WHERE create_time >= date_sub(curdate(), interval 1 day);
+UPDATE agent_family SET publish = true, system_mtime = NOW() WHERE create_time >= date_sub(curdate(), interval 1 day);
+UPDATE agent_person SET publish = true, system_mtime = NOW() WHERE create_time >= date_sub(curdate(), interval 1 day);
+UPDATE subject SET system_mtime = NOW() WHERE create_time >= date_sub(curdate(), interval 1 day);
